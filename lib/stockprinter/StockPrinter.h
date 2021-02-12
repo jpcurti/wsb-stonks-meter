890de31fdb83a144b9f07519788d88a245ffeda2
Curti-Windows\Curti <joaocurti@hotmail.com>
2021-02-12 19:23:55 +0100

    added wsb boot logo

--- lib/stockprinter/StockPrinter.h
+++ lib/stockprinter/StockPrinter.h
@@ -14,13 +14,13 @@
 		StockPrinter();
 
 		void updateDisplay(String message);
+		void drawWSB();
 
 		private:
 		// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 		Adafruit_SSD1306 *display;
 		String stock;
-
+	
 	};
-
-
+	
 #endif
