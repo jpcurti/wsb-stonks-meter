68b7a1ca2b315f4bf88b9266aee26bb0454a58d8
Curti-Windows\Curti <joaocurti@hotmail.com>
2021-02-12 19:05:53 +0100

    added main code for WifiManager and begin of Stock Printer on display

---
+++ lib/stockprinter/StockPrinter.h
@@ -0,0 +1,26 @@
+#ifndef STOCKPRINTER_H
+#define STOCKPRINTER_H
+
+	#include <Wire.h>
+	#include <Adafruit_GFX.h>
+	#include<Adafruit_SSD1306.h>
+	#define SCREEN_WIDTH 128 // OLED display width, in pixels
+	#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+	
+	class StockPrinter{
+
+		public:
+		StockPrinter();
+
+		void updateDisplay(String message);
+
+		private:
+		// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+		Adafruit_SSD1306 *display;
+		String stock;
+
+	};
+
+
+#endif
